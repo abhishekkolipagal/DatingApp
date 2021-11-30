bde4a9afa98a7904db3b01614a46391d385d29ca
abhishek <abhishekkoli3231@gmail.com>
2021-11-30 21:18:10 +0530

    End of section 2

---
+++ api/Controllers/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (4,"abhi");
+
+
+INSERT INTO Users (Id, UserName)
+VALUES (5,"ris");
+
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (6,"anj");
+
